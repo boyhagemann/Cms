c387d164ae99186628abe74ce02fd328ccdb840c
= <=>
2013-11-08 15:23:51 +0100

    got events from content package

--- app/events.php
+++ app/events.php
@@ -8,6 +8,12 @@ Event::subscribe('Boyhagemann\Admin\Subscriber\AddGenerateFrontHookToResource');
 Event::subscribe('Boyhagemann\Admin\Subscriber\AddGenerateAdminHookToResource');
 
 
+
+Event::subscribe('Boyhagemann\Content\Subscriber\AddContentOnResourcePage');
+Event::subscribe('Boyhagemann\Content\Subscriber\AddContentOnPage');
+Event::subscribe('Boyhagemann\Content\Subscriber\HandleRedirectResponse');
+
+
 Event::listen('crud::saved', function($model, $controller) {
 
 	if(!$controller instanceof ResourceController) {
