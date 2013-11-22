dc31f96641d9df337b4213391f8a0f691b2051ec
= <=>
2013-11-22 17:24:06 +0100

    update events

--- app/events.php
+++ app/events.php
@@ -1,24 +1,44 @@
 <?php
 
 use Boyhagemann\Admin\Controller\ResourceController;
-use Boyhagemann\Admin\Model\Resource;
 
 
+/**
+ * Model hooks
+ */
+Event::subscribe('Boyhagemann\Model\Subscriber\GenerateModelAndRepository');
+
+/**
+ * Crud hooks
+ */
+Event::subscribe('Boyhagemann\Crud\Subscriber\BuildModelWhenFormIsReady');
+
+/**
+ * Admin hooks
+ */
 Event::subscribe('Boyhagemann\Admin\Subscriber\AddControllerAndPathsToResource');
 Event::subscribe('Boyhagemann\Admin\Subscriber\AddGenerateFrontHookToResource');
 Event::subscribe('Boyhagemann\Admin\Subscriber\AddGenerateAdminHookToResource');
 Event::subscribe('Boyhagemann\Admin\Subscriber\SwitchContentMode');
+Event::subscribe('Boyhagemann\Admin\Subscriber\ShowHelpPageWhenResourceHasNoFormElements');
 
-
-
+/**
+ * Content hooks
+ */
 Event::subscribe('Boyhagemann\Content\Subscriber\AddContentOnResourcePage');
 Event::subscribe('Boyhagemann\Content\Subscriber\AddContentOnPage');
 Event::subscribe('Boyhagemann\Content\Subscriber\HandleRedirectResponse');
 Event::subscribe('Boyhagemann\Content\Subscriber\ChangeCrudTitle');
 Event::subscribe('Boyhagemann\Content\Subscriber\ReorderBlocksInSection');
 
+/**
+ * Navigation hooks
+ */
 Event::subscribe('Boyhagemann\Navigation\Subscriber\AddResourceLeftRightNavigation');
 
+/**
+ * Text hooks
+ */
 Event::subscribe('Boyhagemann\Text\Subscriber\AddTextDirectlyFromSection');
 
 
